0438989d0264e09a7ffcf056c6210a20e88cab93
Sean Shubin <sshubin@cj.com>
2023-08-13 08:32:15 -0700

    Code Structure Sample Elixir

---
+++ lib/code_structure_sample_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CodeStructureSampleElixir do
+  @moduledoc """
+  Documentation for `CodeStructureSampleElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodeStructureSampleElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/code_structure_sample_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CodeStructureSampleElixirTest do
+  use ExUnit.Case
+  doctest CodeStructureSampleElixir
+
+  test "greets the world" do
+    assert CodeStructureSampleElixir.hello() == :world
+  end
+end
